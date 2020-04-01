fa2c6d26a97b25e87e7e563bae5992bc2c23c2df
yeonjeseo <yeonjeseo@hotmail.com>
2020-04-01 16:44:36 +0900

    2020.04.01 JSX

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    const name = "Yeon Jeffrey Seo";
+    const value = 1;
+    const style = {
+        backgroundColor: "black",
+        padding: "16px",
+        color: "white",
+        fontSize: "12px"
+    };
+
+    return (
+        <Fragment>
+            <div>
+                <input type='text'></input>
+            </div>
+            <div>blahblah</div>
+            <div>Hello {name}</div>
+            <div>{1 + 1 === 3 ? <span>true</span> : <span>false</span>}</div>
+            <div style={style}>{1 + 5 === 6 && <div>맞아요!</div>}</div>
+            <div>
+                {(function() {
+                    if (value === 1) return <div>하나!</div>;
+                    if (value === 2) return <div>둘!</div>;
+                    if (value === 3) return <div>셋</div>;
+                })()}
+            </div>
+            {/* 익명 함수 */}
+            <div>
+                {(() => {
+                    if (value === 1) return <div>하나!</div>;
+                    if (value === 2) return <div>둘!</div>;
+                    if (value === 3) return <div>셋!</div>;
+                })()}
+            </div>
+            <div className='App'>안녕하세요!</div>
+        </Fragment>
+    );
+}
+
+export default App;
